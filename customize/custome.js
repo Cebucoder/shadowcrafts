6cbc1bea20fbb8a13701944fa65f82c117b19afa
CebuCoder <55612034+Cebucoder@users.noreply.github.com>
2023-11-06 15:20:46 +0800

    Update custome.js

--- customize/custome.js
+++ customize/custome.js
@@ -14,15 +14,34 @@ let insetButton = document.getElementById('toggle-inset');
     const blurInput = document.getElementById('blur');
     const opacityInput = document.getElementById('opacity');
     const colorInput = document.getElementById('color');
+    const spreadInput = document.getElementById('spread')
+
+    function getColor(hex, opacity) {
+        var rgb = hexToRgb(hex);
+        var rgbString = "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + "," + opacity + ")";
+        return rgbString;
+      }
+      
+      function hexToRgb(hex) {
+          var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+          return result ? {
+              r: parseInt(result[1], 16),
+              g: parseInt(result[2], 16),
+              b: parseInt(result[3], 16)
+          } : null;
+      }   
 
     function updateBoxShadow() {
+
         const horizontal = horizontalInput.value + 'px';
         const vertical = verticalInput.value + 'px';
         const blur = blurInput.value + 'px';
-        const opacity = opacityInput.value + 'px';
+        const opacity = opacityInput.value;
+        const spread = spreadInput.value + 'px';
         const color = colorInput.value;
         const insetProperty = inset ? 'inset ' : ''; // Include the 'inset' property based on the 'inset' variable
-        const boxShadow = `${insetProperty}${horizontal} ${vertical} ${blur} ${opacity} ${color}`;
+        const rgbaColor = getColor(color, opacity); // Convert hex to RGBA
+        const boxShadow = `${insetProperty}${horizontal} ${vertical} ${blur} ${spread} ${rgbaColor}`;
         box.style.boxShadow = boxShadow;
         console.log(boxShadow);
         document.getElementById('shadow-result').textContent = 'box-shadow: ' +  boxShadow + ';';
@@ -30,13 +49,18 @@ let insetButton = document.getElementById('toggle-inset');
         document.getElementById('vrPX').textContent = vertical;
         document.getElementById('brPX').textContent = blur;
         document.getElementById('orPX').textContent = opacity;
+        document.getElementById('srPX').textContent = spread;
     }
+
+
+   
 
     horizontalInput.addEventListener('input', updateBoxShadow);
     verticalInput.addEventListener('input', updateBoxShadow);
     blurInput.addEventListener('input', updateBoxShadow);
     opacityInput.addEventListener('input', updateBoxShadow);
     colorInput.addEventListener('input', updateBoxShadow);
+    spreadInput.addEventListener('input', updateBoxShadow);
 
     // Initial update
     updateBoxShadow();
